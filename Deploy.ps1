5be7a014bb417d21a1e8a02a9673d1b037b95379
Amal Abeygunawardana <amalabey@gmail.com>
2021-03-05 09:47:50 +1100

    Change deploy script to use cloudfront

--- Deploy.ps1
+++ Deploy.ps1
@@ -7,7 +7,9 @@ param (
     [string]
     $Region = "us-east-1",
     [Parameter()]
-    [switch]$Build
+    [switch]$Build,
+    [Parameter()]
+    [switch]$UseCloudFront
 )
 
 if ($Build -eq $True) {
@@ -18,14 +20,28 @@ if ($Build -eq $True) {
 Write-Host "Creating/Ensuring S3 bucket"
 aws s3api create-bucket --bucket $BucketName --region $Region
 
-Write-Host "Enabling website hosting"
-aws s3 website "s3://$($BucketName)/" --index-document index.html --error-document error.html
-
 Write-Host "Enabling public access to bucket contents via policy"
 aws s3api put-bucket-policy --bucket $BucketName --policy file://aws/bucket-policy.json
 
 Write-Host "Copying website artefacts"
 aws s3 cp .\dist\todo-app\ "s3://$($BucketName)" --recursive
 
-Write-Host "Done. webiste-url: https://$($BucketName).s3.amazonaws.com"
+
+if($UseCloudFront -eq $false) {
+    Write-Host "Enabling website hosting"
+    aws s3 website "s3://$($BucketName)/" --index-document index.html --error-document error.html
+    Write-Host "Done. webiste-url: https://$($BucketName).s3.amazonaws.com"
+}else {
+    Write-Host "Checking if CloudFront distribution exists"
+    $listResult = aws cloudfront list-distributions --query "DistributionList.Items[?contains(Origins.Items[*].DomainName, '$($BucketName).s3.amazonaws.com')].DomainName" | ConvertFrom-Json
+    if($listResult.Count -gt 0) {
+        Write-Host "Done. CloudFront distribution already exists"
+        Write-Host "website-url: https://$($listResult[0])"
+    }else {
+        Write-Host "Creating CloudFront distribution"
+        $createResult = aws cloudfront create-distribution --origin-domain-name "$($BucketName).s3.amazonaws.com" --default-root-object index.html | ConvertFrom-Json
+        Write-Host "Done. webisite-url: https://$($createResult.Distribution.DomainName)"    
+    }
+}
+
 
